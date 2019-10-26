29a29ac41d22f387bad90dfb74e7b17633abe650
Dalton Gray <daltonagray@gmail.com>
2019-10-26 23:19:28 +0100

    Created grid search version of model

--- Gradient_Boosting_Regressor/GridSearchModel.py
+++ Gradient_Boosting_Regressor/GridSearchModel.py
@@ -5,6 +5,8 @@ from sklearn import ensemble
 from sklearn.metrics import mean_absolute_error
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
+# Newly added library
+from sklearn.model_selection import GridSearchCV
 
 # Read in data from CSV as Pandas dataframe
 df = pd.read_csv('~/Downloads\Melbourne_housing_FULL.csv')
@@ -38,26 +40,36 @@ y = df['Price'].values
 #Split the data into test/train set (70/30 split) and shuffle
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, shuffle = True)
 
-# Select algoritm and set hyperparameters
-model = ensemble.GradientBoostingRegressor(
-    n_estimators = 250,
-    learning_rate = 0.1,
-    max_depth = 5,
-    min_samples_split = 4,
-    min_samples_leaf = 6,
-    max_features = 0.6,
-    loss = 'huber'
-)
+# Select algoritm
+model = ensemble.GradientBoostingRegressor()
 
-# Commence training process
-model.fit(X_train, y_train)
+# Set configurations you wish to test for hyperparameters
 
+param_grid = {
+	'n_estimators': [300, 600],
+    'learning_rate': [0.01, 0.02],
+    'max_depth': [7, 9],
+    'min_samples_split': [3, 4],
+    'min_samples_leaf': [5, 6],
+    'max_features' : [0.8, 0.9],
+    'loss': ['ls', 'lad', 'huber']
+}
+
+# Define grid search. Run with n_jobs number of CPUs in parallel
+# n_jobs = -1 makes use of all cores
+gs_cv = GridSearchCV(model, param_grid, n_jobs = -1)
+
+# Run grid search on training data
+gs_cv.fit(X_train, y_train)
+
+# Print optimal hyperparameters
+print (gs_cv.best_params_)
+
+# Check model accuracy (up to two decimal places)
 # Evaluate results (training)
 mse = mean_absolute_error(y_train, model.predict(X_train))
-
 print("Training Set mean Absolute Error: %.2f" % mse)
 
 # Evaluate results (test)
 mse = mean_absolute_error(y_test, model.predict(X_test))
-
 print("Test Set mean Absolute Error: %.2f" % mse)
