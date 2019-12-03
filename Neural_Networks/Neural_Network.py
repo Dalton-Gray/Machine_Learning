6108b84f089110b2894fd480e15939450a64fca0
Dalton Gray <daltonagray@gmail.com>
2019-12-03 22:35:58 +0000

    Neural_Network.py bug fixes

--- Neural_Networks/Neural_Network.py
+++ Neural_Networks/Neural_Network.py
@@ -5,10 +5,10 @@ class NeuralNetwork():
 	def __init__(self):
 		np.random.seed(1)
 
-		self.synaptic_weights = 2 * np.random((3, 1)) -1
+		self.synaptic_weights = 2 * np.random.random((3, 1)) -1
 
 	def sigmoid(self, x):
-		return 1/ (1 + np.exp(-x))
+		return 1 / (1 + np.exp(-x))
 
 	def sigmoid_derivative(self, x):
 		return x * (1 - x)
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 	neural_network = NeuralNetwork()
 
 	print("Random synaptic weights: ")
-	print(neural_network.self.synaptic_weights)
+	print(neural_network.synaptic_weights)
 
 	training_inputs = np.array([[0,0,1],
 							[1,1,1],
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 	neural_network.train(training_inputs, training_outputs, 100000)
 
 	print("Synaptic weights after training: ")
-	print(neural_network.self.synaptic_weights)
+	print(neural_network.synaptic_weights)
 
 	A = str(input("Input 1: "))
 	B = str(input("Input 2: "))
